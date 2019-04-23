139d0c21befb9334eae57622f1e3cf4b0fbc123f
youssef hoummad <youssef.hoummad@outlook.com>
2019-04-23 18:18:10 +0000

    add login route and login.html

--- src/routes.py
+++ src/routes.py
@@ -2,9 +2,15 @@ from flask import render_template
 
 from src import app
 from src import fake_db
+from src import forms
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template('index.html', posts=fake_db.posts)
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html', form=forms.LoginForm())
