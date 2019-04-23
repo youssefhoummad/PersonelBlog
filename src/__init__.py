ebcbb9966ea4d5cf4e15c74d392c970fac392b68
youssef hoummad <youssef.hoummad@outlook.com>
2019-04-23 17:40:04 +0000

    create blank flask app

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'f2c80b32f536165d6a57ed9cb08a84a4'
+
+# db = SQLAlchemy(app)
+
+
+
+from src import routes
+# from src.models import *
---
+++ src/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template
+
+from src import app
+
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
