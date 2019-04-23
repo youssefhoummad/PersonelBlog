4e37f69ac31bde7b09c4d69d5ae7530b2f44acca
youssef hoummad <youssef.hoummad@outlook.com>
2019-04-23 17:49:12 +0000

    add fake posts and loop in index

--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template
 
 from src import app
-
+from src import fake_db
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=fake_db.posts)
