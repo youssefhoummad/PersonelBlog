ab59e8ee45b886f6e666f15729b3c94bd8671929
youssef hoummad <youssef.hoummad@outlook.com>
2019-04-23 18:14:08 +0000

    add LoginForm to permit loign user

--- src/forms.py
+++ src/forms.py
@@ -1,4 +1,11 @@
-# from flask_wtf import FlaskForm
-# from wtforms import Form, StringField, PasswordField, SubmitField, BooleanField, ValidationError
-# from wtforms.validators import Length, DataRequired, Email, EqualTo
+from flask_wtf import FlaskForm
+from wtforms import Form, StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Length, DataRequired, Email, EqualTo
 
+
+
+class LoginForm(FlaskForm):
+    username = StringField('اسم المستخدم', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('كلمة المرور', validators=[DataRequired()])
+    remember = BooleanField('تذكرني')
+    submit = SubmitField('دخول')
