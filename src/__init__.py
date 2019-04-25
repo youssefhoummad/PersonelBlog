e24efbdc6aced44e078f253e515858868a787fc7
youssef hoummad <youssef.hoummad@outlook.com>
2019-04-25 17:28:41 +0000

    feat: add login manager

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 
@@ -8,8 +9,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'f2c80b32f536165d6a57ed9cb08a84a4'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
+login_manager.login_view = 'login'              # if login required redierct to login
+login_manager.login_message_category = 'info'   # class bootstrap for div flashs
 
 
 from src import routes
-# from src.models import *
+from src.models import *
