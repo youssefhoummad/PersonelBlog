a504495b91dbb7a6b5603e23b476656d5611725c
youssef hoummad <youssef.hoummad@outlook.com>
2019-04-25 17:32:49 +0000

    feat: add funcs to manipulate db

--- src/models.py
+++ src/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
-from src import db
+from src import db, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -27,3 +29,28 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % self.title
+
+
+
+
+# some function to maniplt db
+def get_users():
+    return User.query.all()
+
+def get_posts():
+    return Post.query.all()
+
+
+@login_manager.user_loader
+def get_user(user_id):
+    return User.query.get(int(user_id))
+
+def set_user(username, email, password):
+    db.session.add(User(username=username, email=email, password=password))
+    db.session.commit()
+
+
+def set_post(title, content, username_id=None):
+    db.session.add(Post(title=title, content=content, \
+                        username_id=int(username_id) if username_id else 1))
+    db.session.commit()
